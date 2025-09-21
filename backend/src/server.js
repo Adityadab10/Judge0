61bbba75b2cb7b03359bff292efaf9a3e57bc6e2
Adityadab10 <adityadab27@gmail.com>
2025-09-21 20:03:00 +0530

    backend docker container working and connected

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const codeRoutes = require('./src/routes/code.routes');
+const codeRoutes = require('./routes/code.routes');
 
 dotenv.config();
 
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000','http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json({ limit: '1mb' }));
 
 // Routes
@@ -19,19 +22,10 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong!' });
 });
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("MongoDB connected...");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => console.error(err));
 
-// backend/server.js
+const PORT = process.env.PORT || 8080;
 
-app.use(cors({
-  origin: ['http://localhost:3000','http://localhost:5173'],
-  credentials: true
-}))
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
